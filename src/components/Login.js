6ec252274ce8bf2061701a8020560e3cb9e564e4
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-05-29 23:32:42 +0100

    ref correted

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Link} from 'react-router-dom'
+// import {BrowserRouter as Router, Link} from 'react-router-dom'
 import Button from '../assests/images/Button'
 import './Login.css'
 
